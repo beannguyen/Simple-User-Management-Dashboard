6550dfceee9982ce6cc5329ec64f5a8bd397b3b8
Phillip Truong <truonghoangvinhphuc@gmail.com>
2020-07-08 16:35:29 +0300

    Add Login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
+import { Router } from  '@angular/router';
+import { User } from  '../user';
+import { AuthService } from  '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
