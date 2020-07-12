8502ca60f0057ae31417ed05a360ebac324918e1
Phillip Truong <truonghoangvinhphuc@gmail.com>
2020-07-12 05:37:55 +0300

    Re-init project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simple-user-management-dashboard'`, () => {
+  it(`should have as title 'user-management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simple-user-management-dashboard');
+    expect(app.title).toEqual('user-management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('simple-user-management-dashboard app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('user-management app is running!');
   });
 });
