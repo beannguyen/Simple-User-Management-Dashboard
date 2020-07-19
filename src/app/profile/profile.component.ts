f310d0c2b15c54efd9cd72be4b3d228f77f45810
Phillip Truong <truonghoangvinhphuc@gmail.com>
2020-07-19 05:28:47 +0300

    Update Profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,14 +6,33 @@ import { TokenStorageService } from '../_services/token-storage.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
+
 export class ProfileComponent implements OnInit {
 
   currentUser: any;
 
+  demoUser: User = {
+    username: 'Vinh Phuc',
+    email: 'phuc@ynwa.com',
+    age: 24,
+    image: '../assets/demo_profile.jpg',
+    roles: "Developer"
+  };
+
   constructor(private token: TokenStorageService) { }
 
   ngOnInit(): void {
-    this.currentUser = this.token.getUser();
+    // this.currentUser = this.token.getUser();
+    this.currentUser = this.demoUser;
   }
 
+}
+
+export class User {
+  username: string;
+  email: string;
+  age: number;
+  image: string;
+  roles: string;
+  constructor() {}
 }
